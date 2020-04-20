6af5b510923e885e0aef56adba4bcc68ffc8b257
Miguel Romariz <up201708809@fe.up.pt>
2020-04-20 17:53:35 +0000

    Added B2node

---
+++ src/javacc/nodes/B2node.java
@@ -0,0 +1,28 @@
+package javacc.nodes;
+
+public class B2node extends InstructionNode {
+    private int literal;
+    private String r1;
+
+    public B2node(String Address, String instruction, String r1, int literal) {
+        super(Address, instruction);
+        this.r1 = r1;
+        this.literal = literal;
+    }
+
+    public int getLiteral() {
+        return literal;
+    }
+
+    public void setLiteral(int literal) {
+        this.literal = literal;
+    }
+
+    public String getR1() {
+        return r1;
+    }
+
+    public void setR1(String r1) {
+        this.r1 = r1;
+    }
+}
