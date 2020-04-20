7d5b7e5644ec0606d1a428255fb0b294720cd57a
Miguel Romariz <up201708809@fe.up.pt>
2020-04-20 17:50:59 +0000

    Added A2node

---
+++ src/javacc/nodes/A2node.java
@@ -0,0 +1,29 @@
+package javacc.nodes;
+
+public class A2node extends InstructionNode{
+    private String r1;
+    private String r2;
+
+
+    public A2node(String Address, String instruction, String r1, String r2) {
+        super(Address, instruction);
+        this.r1 = r1;
+        this.r2 = r2;
+    }
+
+    public String getR1() {
+        return r1;
+    }
+
+    public void setR1(String r1) {
+        this.r1 = r1;
+    }
+
+    public String getR2() {
+        return r2;
+    }
+
+    public void setR2(String r2) {
+        this.r2 = r2;
+    }
+}
